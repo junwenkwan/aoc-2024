6247abd4791540ebd0171d29ec5ad8628d44fe5b
Jun Kwan <kwanjunwen@gmail.com>
2024-12-07 00:00:19 +1100

    Day 6 sol

--- day_06.py
+++ day_06.py
@@ -13,137 +13,89 @@ def find_guard_pos(grid):
                 return r, c, "v"
     return -1, -1, "X"
 
-def traverse_up(grid, r, c, possible_pos):
-    while r >= 0:
-        if grid[r][c] == "#":
-            break
-        possible_pos.add((r,c))
-        r -= 1
-    return r, c, possible_pos
-
-def traverse_down(grid, r, c, possible_pos):
-    while r < len(grid):
-        if grid[r][c] == "#":
-            break
-        possible_pos.add((r,c))
-        r += 1
-    return r, c, possible_pos
-
-def traverse_left(grid, r, c, possible_pos):
-    while c >= 0:
-        if grid[r][c] == "#":
-            break
-        possible_pos.add((r,c))
-        c -= 1
-    return r, c, possible_pos
-
-def traverse_right(grid, r, c, possible_pos):
-    while c < len(grid[0]):
-        if grid[r][c] == "#":
-            break
-        possible_pos.add((r,c))
-        c += 1
-    return r, c, possible_pos
-
-def start(x, y, dir, grid, NUM_ROW, NUM_COL, obstacles, guard_possible_pos):
+def start(x, y, dir, grid, NUM_ROW, NUM_COL):
+    tiles = set()
     is_loop = False
-    
+    tiles_dir = set()
+
     while True:
-        if x < 0 or y < 0 or x >= NUM_ROW-1 or y >= NUM_COL-1:
-            break
+        tiles.add((x,y))
+        tiles_dir.add((x,y,dir))
 
         if dir == "^":
-            o_x, o_y, guard_possible_pos = traverse_up(grid, x, y, guard_possible_pos)
-            dir = ">" # turn right 90 deg
-            obstacles.add((o_x, o_y))
-            x = o_x + 1
+            x = x - 1
         elif dir == ">":
-            o_x, o_y, guard_possible_pos = traverse_right(grid, x, y, guard_possible_pos)
-            dir = "v" # turn right 90 deg
-            obstacles.add((o_x, o_y))
-            y = o_y - 1
+            y = y + 1
         elif dir == "v":
-            o_x, o_y, guard_possible_pos = traverse_down(grid, x, y, guard_possible_pos)
-            dir = "<" # turn right 90 deg
-            obstacles.add((o_x, o_y))
-            x = o_x - 1
+            x = x + 1             
         elif dir == "<":
-            o_x, o_y, guard_possible_pos = traverse_left(grid, x, y, guard_possible_pos)
-            dir = "^" # turn right 90 deg
-            obstacles.add((o_x, o_y))
-            y = o_y + 1
+            y = y - 1
 
-    return is_loop, guard_possible_pos
+        if x < 0 or y < 0 or x >= NUM_ROW or y >= NUM_COL:
+            break
+
+        if (x,y,dir) in tiles_dir:
+            is_loop = True
+            break
+        
+        if grid[x][y] == "#":
+            if dir == "^":
+                x = x + 1
+                dir = ">"
+            elif dir == ">":
+                y = y - 1 
+                dir = "v"
+            elif dir == "v":
+                x = x - 1
+                dir = "<" 
+            elif dir == "<":
+                y = y + 1
+                dir = "^"
+
+    return tiles, is_loop
 
 def main():
     # part 1
     with open("./input.txt", "r") as f:
         lines = f.readlines()
     grid = [list(row.rstrip()) for row in lines]
-
-    grid = ["....#.....",
-            ".........#",
-            "..........",
-            "..#.......",
-            ".......#..",
-            "..........",
-            ".#..^.....",
-            "........#.",
-            "#.........",
-            "......#..."]
     
     grid = [list(row) for row in grid]
-
+    
     NUM_ROW = len(grid)
     NUM_COL = len(grid[0])
 
     x_init, y_init, dir_init = find_guard_pos(grid)
     print("Starting position:", x_init, y_init, dir_init)
 
-    guard_possible_pos = set()
-    obstacles = set()
-    _, guard_possible_pos = start(x_init, y_init, dir_init, grid, NUM_ROW, NUM_COL, obstacles, guard_possible_pos)
-    print(len(guard_possible_pos))
+    tiles, _ = start(x_init, y_init, dir_init, grid, NUM_ROW, NUM_COL)
+    
+    print(len(tiles))
 
     # part 2
-    place_obstacles = guard_possible_pos.copy()
+    place_obstacles = tiles.copy()
     sum_loop = 0
-    obstacles = set()
     replace = False
-    for r_o, c_o in tqdm(place_obstacles):
-        if r_o == x_init + 1 and c_o == y_init + 1:
-            continue
 
+    for item in tqdm(place_obstacles):
+        r_o = item[0]
+        c_o = item[1]
         if grid[r_o][c_o] == ".":
             grid[r_o][c_o] = "#"
             replace = True
         else:
             continue
-        is_loop = start(x, y, dir, grid, NUM_ROW, NUM_COL, set(), set())
+
+        _, is_loop = start(x_init, y_init, dir_init, grid, NUM_ROW, NUM_COL)
+        
         if replace:
             grid[r_o][c_o] = "."
             replace = False
+
         if is_loop:
             sum_loop += 1
 
-    # for r_o in tqdm.tqdm(range(NUM_ROW)):
-    #     for c_o in range(NUM_COL):
-    #         if grid[r_o][c_o] == ".":
-    #             grid[r_o][c_o] = "#"
-    #             replace = True
-    #         else:
-    #             continue
-    #         obstacles = set()
-    #         is_loop = start(x, y, dir, grid, NUM_ROW, NUM_COL, obstacles, guard_possible_pos)
-    #         if replace:
-    #             grid[r_o][c_o] = "."
-    #             replace = False
-    #         if is_loop:
-    #             sum_loop += 1
-    #             print(f"Placing obs at {r_o},{c_o} will create a loop, total loop found {sum_loop}")
-    
-    # print(sum_loop)
-    # 1920 too high
+    print(sum_loop)
 
 if __name__ == "__main__":
     main()
